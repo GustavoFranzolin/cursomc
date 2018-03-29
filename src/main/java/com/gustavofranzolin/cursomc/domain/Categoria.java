389bc836ddb383cb377dc6559e36896b610bc1c1
Gustavo Franzolin <gustavo.franzolin@gmail.com>
2018-03-29 16:12:43 -0300

    Banco de dados H2 e criacao automatica da base dados

--- src/main/java/com/gustavofranzolin/cursomc/domain/Categoria.java
+++ src/main/java/com/gustavofranzolin/cursomc/domain/Categoria.java
@@ -2,11 +2,23 @@ package com.gustavofranzolin.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
 public class Categoria implements Serializable{
 	private static final long serialVersionUID = 1L;
 
+	//Id e Generated são anotações utilizadas para gerar a chave primária no banco de dados H2
+	//Dependendo do BD, pode ser que tenha que utilizar outro GenerationType
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
+	
 	private String  nome;
 
 
